997fea888524a9aee2c60604e06e2943f487e03d
Mateusz Lesiak <210248@edu.p.lodz.pl>
2019-05-12 12:18:10 +0200

    Add info about vehicle to app.

--- src/main/java/com/pracownia/vanet/Simulation.java
+++ src/main/java/com/pracownia/vanet/Simulation.java
@@ -3,179 +3,167 @@ package com.pracownia.vanet;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class Simulation implements Runnable{
+public class Simulation implements Runnable {
 
-	private Map map;
-	private List<Circle> circleList;
-	private List<Circle> rangeList;
-	private List<Label> labelList;
-	private List<Circle> stationaryCirclelist;
+    public Boolean simulationRunning;
+    Thread tr;
+    private Map map;
+    private List<Circle> circleList;
+    private List<Circle> rangeList;
+    private List<Label> labelList;
+    private List<Circle> stationaryCirclelist;
 
-	Thread tr;
-	public Simulation() {
-		map = new Map();
-		circleList = new ArrayList<>();
-		rangeList = new ArrayList<>();
-		labelList = new ArrayList<>();
-		stationaryCirclelist = new ArrayList<>();
+    public Simulation() {
+        map = new Map();
+        circleList = new ArrayList<>();
+        rangeList = new ArrayList<>();
+        labelList = new ArrayList<>();
+        stationaryCirclelist = new ArrayList<>();
+        this.simulationRunning = true;
+        tr = new Thread(this);
+    }
 
-		tr = new Thread(this);
-	}
+
+    public void run() {
+        while (true) {
+            if (simulationRunning) {
+                updateVehiclesPosition();
+                checkVehicleCrossing();
+                resetReferences();
+                checkVehicleEventSource();
+                updateStationaryPoints();
+
+                //showVehiclesConnected();
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    private void updateVehiclesPosition() {
+        int it = 0;
+
+        for (Vehicle vehicle : map.getVehicles()) {
+            vehicle.update(map);
+            try {
+                double vehicleX = vehicle.getCurrentLocation().getX();
+                double vehicleY = vehicle.getCurrentLocation().getY();
+                circleList.get(it).setCenterX(vehicleX);
+                circleList.get(it).setCenterY(vehicleY);
+
+                rangeList.get(it).setCenterX(vehicleX);
+                rangeList.get(it).setCenterY(vehicleY);
+
+                //labelList.get(it).setText(String.valueOf(vehicle.getCollectedEvents().size()));
+                if (vehicle.getCollectedEvents().size() > 0) {
+                    circleList.get(it).setFill(Color.BROWN);
+                }
+                labelList.get(it).setLayoutX(vehicleX + 7.0);
+                labelList.get(it).setLayoutY(vehicleY);
+            } catch (IndexOutOfBoundsException e) {
+            }
+            it++;
+        }
+    }
+
+    private void checkVehicleCrossing() {
+        for (Vehicle vehicle : map.getVehicles()) {
+            for (Crossing crossing : map.getCrossings()) {
+
+                if (crossing.getDistanceToCrossing(vehicle) < Crossing.DETECTION_RANGE) {
+                    crossing.transportVehicle(vehicle);
+                }
+            }
+        }
+    }
+
+    private void resetReferences() {
+        for (Crossing crossing : map.getCrossings()) {
+            crossing.resetLastTransportedVehicle();
+        }
+    }
+
+    private void checkVehicleEventSource() {
+        checkVehicleEventSourceEncountered();
+        checkVehicleEventSourceCollected();
+    }
+
+    private void checkVehicleEventSourceCollected() {
+        for (Vehicle vehicle : map.getVehicles()) {
+            for (EventSource eventSource : map.getEventSources()) {
+                if (eventSource.isInRange(vehicle.getCurrentLocation())) {
+                    for (Event event : vehicle.getCollectedEvents()) {
+                        if (event.getId() == eventSource.getId()) {
+                            return;
+                        }
+                    }
+
+                    vehicle.getCollectedEvents().add(eventSource.getEvent());
+                }
+            }
+        }
+    }
+
+    private void checkVehicleEventSourceEncountered() {
+        for (Vehicle vehicle : map.getVehicles()) {
+            for (EventSource eventSource : map.getEventSources()) {
+                if (eventSource.isInRange(vehicle.getCurrentLocation())) {
+                    for (Event event : vehicle.getEncounteredEvents()) {
+                        if (event.getId() == eventSource.getId()) {
+                            return;
+                        }
+                    }
+
+                    vehicle.getEncounteredEvents().add(eventSource.getEvent());
+                    System.out.println("Event Encountered by Vehicle");
+                }
+            }
+        }
+    }
+
+    private void updateStationaryPoints() {
+        int it = 0;
+        for (StationaryNetworkPoint s : map.getStationaryNetworkPoints()) {
+            s.update(map);
+            s.checkIfChangeVehicleTrustLevel();
+            try {
+                if (s.getCollectedEvents().size() > 0) { stationaryCirclelist.get(it).setFill(Color.CYAN); }
+            } catch (IndexOutOfBoundsException e) {
+                e.printStackTrace();
+            }
+            it++;
+        }
+    }
+
+    public void switchOffRangeCircles() {
+        for (Circle rangeCircle : rangeList) {
+            rangeCircle.setStroke(Color.TRANSPARENT);
+        }
+    }
+
+    public void switchOnRangeCircles() {
+        for (Circle rangeCircle : rangeList) {
+            rangeCircle.setStroke(Color.BLACK);
+        }
+    }
 
 
-	public void run(){
-		while (true) {
-			updateVehiclesPosition();
-			checkVehicleCrossing();
-			resetReferences();
-			checkVehicleEventSource();
-			updateStationaryPoints();
-
-			//showVehiclesConnected();
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-			}
-		}
-	}
-
-	private void updateVehiclesPosition() {
-
-		int it = 0;
-
-		for (Vehicle vehicle : map.getVehicles()) {
-			vehicle.update(map);
-			try
-			{
-				circleList.get(it).setCenterX(vehicle.getCurrentLocation().getX());
-				circleList.get(it).setCenterY(vehicle.getCurrentLocation().getY());
-				rangeList.get(it).setCenterX(vehicle.getCurrentLocation().getX());
-				rangeList.get(it).setCenterY(vehicle.getCurrentLocation().getY());
-				//labelList.get(it).setText(String.valueOf(vehicle.getCollectedEvents().size()));
-				if(vehicle.getCollectedEvents().size() > 0)
-					circleList.get(it).setFill(Color.BROWN);
-				labelList.get(it).setLayoutX(vehicle.getCurrentLocation().getX() + 7.0);
-				labelList.get(it).setLayoutY(vehicle.getCurrentLocation().getY());
-			}
-			catch (IndexOutOfBoundsException e)
-			{
-				//e.printStackTrace();
-			}
-			it++;
-		}
-	}
-
-	private void checkVehicleCrossing()
-	{
-		for (Vehicle vehicle : map.getVehicles()) {
-			for(Crossing crossing : map.getCrossings()) {
-
-				if(crossing.getDistanceToCrossing(vehicle) < Crossing.DETECTION_RANGE){
-					crossing.transportVehicle(vehicle);
-				}
-			}
-		}
-	}
-
-	private void resetReferences()
-	{
-		for(Crossing crossing : map.getCrossings())
-		{
-			crossing.resetLastTransportedVehicle();
-		}
-	}
-
-	private void checkVehicleEventSource()
-	{
-		checkVehicleEventSourceEncountered();
-		checkVehicleEventSourceCollected();
-	}
-	private void checkVehicleEventSourceCollected()
-	{
-		for (Vehicle vehicle : map.getVehicles())
-		{
-			for (EventSource eventSource : map.getEventSources())
-			{
-				if(eventSource.isInRange(vehicle.getCurrentLocation()))
-				{
-					for (Event event : vehicle.getCollectedEvents())
-					{
-						if(event.getId() == eventSource.getId())
-						{
-							return;
-						}
-					}
-
-					vehicle.getCollectedEvents().add(eventSource.getEvent());
-				}
-			}
-		}
-	}
-	private void checkVehicleEventSourceEncountered()
-	{
-		for (Vehicle vehicle : map.getVehicles())
-		{
-			for (EventSource eventSource : map.getEventSources())
-			{
-				if(eventSource.isInRange(vehicle.getCurrentLocation()))
-				{
-					for (Event event : vehicle.getEncounteredEvents())
-					{
-						if(event.getId() == eventSource.getId())
-						{
-							return;
-						}
-					}
-
-					vehicle.getEncounteredEvents().add(eventSource.getEvent());
-					System.out.println("Event Encountered by Vehicle");
-				}
-			}
-		}
-	}
-	private void updateStationaryPoints()
-	{
-		int it = 0;
-		for(StationaryNetworkPoint s : map.getStationaryNetworkPoints())
-		{
-			s.update(map);
-			s.checkIfChangeVehicleTrustLevel();
-			try
-			{
-				if (s.getCollectedEvents().size() > 0)
-					stationaryCirclelist.get(it).setFill(Color.CYAN);
-			}
-			catch (IndexOutOfBoundsException e)
-			{
-				e.printStackTrace();
-			}
-			it++;
-		}
-	}
-
-	public void switchOffRangeCircles()
-	{
-		for(Circle rangeCircle : rangeList)
-		{
-			rangeCircle.setStroke(Color.TRANSPARENT);
-		}
-	}
-
-	public void switchOnRangeCircles()
-	{
-		for(Circle rangeCircle : rangeList)
-		{
-			rangeCircle.setStroke(Color.BLACK);
-		}
-	}
-
+    public void changeVehiclesRanges(double range) {
+        for (Vehicle vehicle : map.getVehicles()) {
+            vehicle.setRange(range);
+        }
+        for (Circle rangeCircle : rangeList) {
+            rangeCircle.setRadius(range);
+        }
+    }
 /*
 	private void showVehiclesConnected(){
 		int it = 0;
@@ -195,5 +183,4 @@ public class Simulation implements Runnable{
 			it++;
 		}
 	}*/
-
 }
--- src/main/java/com/pracownia/vanet/WindowApp.java
+++ src/main/java/com/pracownia/vanet/WindowApp.java
@@ -13,79 +13,161 @@ import javafx.stage.WindowEvent;
 
 public class WindowApp extends Application {
 
-	private Group root = new Group();
-	ShapesCreator shapesCreator = new ShapesCreator(root);
-	private boolean isRangeRendered = false;
+    public TextField trustLevelField;
+    public TextField speedField;
+    public TextField vehIdField;
+    public TextField connPointsField;
+    private Group root = new Group();
+    private ShapesCreator shapesCreator;
+    private boolean isRangeRendered = false;
+    private Simulation simulation;
 
-	public static void main(String[] args){
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent e) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
 
-		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			@Override
-			public void handle(WindowEvent e) {
-				Platform.exit();
-				System.exit(0);
-			}
-		});
+        this.simulation = new Simulation();
+        this.shapesCreator = new ShapesCreator(root, this.simulation, this);
 
-		Simulation simulation = new Simulation();
+        shapesCreator.setRoutesLines(simulation);
+        shapesCreator.setSourceEventCircles(simulation);
+        shapesCreator.setStationaryPointCircles(simulation);
 
-		shapesCreator.setRoutesLines(simulation);
-		shapesCreator.setSourceEventCircles(simulation);
-		shapesCreator.setStationaryPointCircles(simulation);
+        setInterface(simulation);
 
-		setInterface(simulation);
-
-		Scene scene = new Scene(root, 1100, 800);
-		primaryStage.setTitle("Vanet");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		simulation.tr.start();
-	}
-
-
-
-	private void setInterface(Simulation simulation)
-	{
-		Button showRangeButton = new Button("Show Range");
-		Button spawnVehiclesButton = new Button("Spawn Vehicles");
-		TextField vehiclesAmountField = new TextField();
-		Label vehiclesAmountLabel = new Label("Vehicle Amount");
-
-		showRangeButton.setLayoutX(950.0);
-		showRangeButton.setLayoutY(100.0);
-		spawnVehiclesButton.setLayoutX(950.0);
-		spawnVehiclesButton.setLayoutY(130.0);
-		vehiclesAmountLabel.setLayoutX(950.0);
-		vehiclesAmountLabel.setLayoutY(170.0);
-		vehiclesAmountField.setLayoutX(950.0);
-		vehiclesAmountField.setLayoutY(190.0);
-;
-		showRangeButton.setOnAction(e -> {
-			isRangeRendered = !isRangeRendered;
-			if(isRangeRendered)
-			{
-				simulation.switchOnRangeCircles();
-			}
-			else
-			{
-				simulation.switchOffRangeCircles();
-			}
-		});
-
-		spawnVehiclesButton.setOnAction(e -> {
-			simulation.getMap().addVehicles(Integer.parseInt(vehiclesAmountField.getText()));
-			shapesCreator.setVehicleCircles(simulation, Integer.parseInt(vehiclesAmountField.getText()));
-			shapesCreator.setLabels(simulation, Integer.parseInt(vehiclesAmountField.getText()));
-
-		});
-
-		root.getChildren().addAll(showRangeButton, spawnVehiclesButton, vehiclesAmountField, vehiclesAmountLabel);
-	}
+        Scene scene = new Scene(root, 1100, 800);
+        primaryStage.setTitle("Vanet");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        simulation.tr.start();
+    }
 
 
+    private void setInterface(Simulation simulation) {
+        Button showRangeButton = new Button("Show Range");
+        Button changeRangeButton = new Button("ChangeRange");
+        Button spawnVehiclesButton = new Button("Spawn Vehicles");
+        TextField vehiclesAmountField = new TextField();
+        TextField rangeAmountField = new TextField();
+        Label rangeAmountLabel = new Label("Range");
+        Label vehiclesAmountLabel = new Label("Vehicle Amount");
+
+        // Start stop simulation.
+        Button startSimulation = new Button("Start simulation");
+        startSimulation.setLayoutX(950.0);
+        startSimulation.setLayoutY(280.);
+        startSimulation.setOnAction(e -> {
+            simulation.setSimulationRunning(true);
+        });
+
+        Button stopSimulation = new Button("Stop simulation");
+        stopSimulation.setLayoutX(950.0);
+        stopSimulation.setLayoutY(310.);
+        stopSimulation.setOnAction(e -> {
+            simulation.setSimulationRunning(false);
+        });
+
+
+        // Vehicle informations.
+        this.trustLevelField = new TextField();
+        trustLevelField.setLayoutX(950.0);
+        trustLevelField.setLayoutY(400.0);
+
+        Label trustLevelLabel = new Label("Trust level");
+        trustLevelLabel.setLayoutX(950.0);
+        trustLevelLabel.setLayoutY(430.0);
+
+        this.speedField = new TextField();
+        speedField.setLayoutX(950.0);
+        speedField.setLayoutY(460.0);
+
+        Label speedLabel = new Label("Speed");
+        speedLabel.setLayoutX(950.0);
+        speedLabel.setLayoutY(490.0);
+
+        this.vehIdField = new TextField();
+        vehIdField.setLayoutX(950.0);
+        vehIdField.setLayoutY(520.0);
+
+        Label vehIdLabel = new Label("Veh id");
+        vehIdLabel.setLayoutX(950.0);
+        vehIdLabel.setLayoutY(550.0);
+
+
+        this.connPointsField = new TextField();
+        connPointsField.setLayoutX(950.0);
+        connPointsField.setLayoutY(580.0);
+
+        Label connPointsLabel = new Label("Conn points");
+        connPointsLabel.setLayoutX(950.0);
+        connPointsLabel.setLayoutY(610.0);
+
+        // Other stuff.
+
+        showRangeButton.setLayoutX(950.0);
+        showRangeButton.setLayoutY(80.0);
+
+        changeRangeButton.setLayoutX(950.0);
+        changeRangeButton.setLayoutY(110.0);
+        rangeAmountLabel.setLayoutX(950.0);
+        rangeAmountLabel.setLayoutY(140.0);
+        rangeAmountField.setLayoutX(950.0);
+        rangeAmountField.setLayoutY(160.0);
+        rangeAmountField.setText("40.0");
+
+        spawnVehiclesButton.setLayoutX(950.0);
+        spawnVehiclesButton.setLayoutY(190.0);
+        vehiclesAmountLabel.setLayoutX(950.0);
+        vehiclesAmountLabel.setLayoutY(220.0);
+        vehiclesAmountField.setLayoutX(950.0);
+        vehiclesAmountField.setLayoutY(240.0);
+        vehiclesAmountField.setText("10");
+
+        changeRangeButton.setOnAction(e -> simulation.changeVehiclesRanges(Double.parseDouble(rangeAmountField.getText())));
+
+
+        showRangeButton.setOnAction(e -> {
+            isRangeRendered = !isRangeRendered;
+            if (isRangeRendered) {
+                simulation.switchOnRangeCircles();
+            } else {
+                simulation.switchOffRangeCircles();
+            }
+        });
+
+        spawnVehiclesButton.setOnAction(e -> {
+            simulation.getMap().addVehicles(Integer.parseInt(vehiclesAmountField.getText()));
+            shapesCreator.setVehicleCircles(simulation, Integer.parseInt(vehiclesAmountField.getText()));
+            shapesCreator.setLabels(simulation, Integer.parseInt(vehiclesAmountField.getText()));
+        });
+
+        root.getChildren()
+                .addAll(showRangeButton,
+                        spawnVehiclesButton,
+                        vehiclesAmountField,
+                        stopSimulation,
+                        trustLevelField,
+                        trustLevelLabel,
+                        speedField,
+                        speedLabel,
+                        vehIdField,
+                        vehIdLabel,
+                        connPointsField,
+                        connPointsLabel,
+                        startSimulation,
+                        vehiclesAmountLabel,
+                        rangeAmountLabel,
+                        rangeAmountField,
+                        changeRangeButton);
+    }
 }
